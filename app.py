1d9d53d9bdde66f34413918130a2742c5855e76c
Cletus Toh <cletustoh@fire.local>
2025-01-16 08:48:56 +0800

    ajax test new

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute
 from pynamodb.exceptions import DoesNotExist
